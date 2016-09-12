4c7822a373b3021dba4f39f83adbab32c92496cb
DEEPSANA SHAHI <deepsanashahi@adelphi.internal>
2016-09-12 10:47:54 -0400

    labo4 ticket machine

--- lab04-ticket-machine/TicketMachine.java
+++ lab04-ticket-machine/TicketMachine.java
@@ -9,6 +9,9 @@
  * @author David J. Barnes and Michael Kölling
  * @version 2016.02.29
  */
+//Question 1: How can we tell from just its header that setPrice is a method and not a constructor?
+//public void setPrice(int cost)?
+// Because setPrice is not an object
 public class TicketMachine
 {
     // The price of a ticket from this machine.
@@ -17,19 +20,36 @@ public class TicketMachine
     private int balance;
     // The total amount of money collected by this machine.
     private int total;
+    // The score for the machine
+    private int score;
 
     /**
      * Create a machine that issues tickets of the given price.
      * Note that the price must be greater than zero, and there
      * are no checks to ensure this.
      */
+    public TicketMachine()
+    {
+        price = 1000; //price of ticket is now 1,000 cents.
+        balance = 0;
+        total = 0;
+    }
+    
     public TicketMachine(int cost)
     {
         price = cost;
         balance = 0;
         total = 0;
     }
-
+    
+    /**
+     * Set price that assigns value of its parameter.
+     */
+    public void setPrice(int costTicket)
+    {
+        price = costTicket;
+    }
+    
     /**
      * Return the price of a ticket.
      */
@@ -54,7 +74,38 @@ public class TicketMachine
     {
         balance = balance + amount;
     }
+    
+ /**
+ * Reduce price by the given amount.
+ */
+public void discount(int amount)
+{
+  price = price - amount;
+}
 
+public void prompt()
+{
+ System.out.println("# Please insert the correct amount of money.");
+    
+}
+    
+ public void showPrice()
+    {
+        System.out.println("The price of the ticket is"+price+"cents");
+    }
+   
+   public void empty() //Removing all money from the machine. This method doesn't need any parameters. 
+   {
+       total = 0;
+    }
+    
+    // creating two ticket machine of type x and y.
+    TicketMachine x = new TicketMachine(30);
+    TicketMachine y = new TicketMachine(20);
+    
+    x.showPrice();
+    y.showPrice();
+    
     /**
      * Print a ticket.
      * Update the total collected and
